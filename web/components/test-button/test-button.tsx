cc2d0f56a9abd7ddeebd1ec0d96872554baf1035
risonakamo <hmykanae@gmail.com>
2024-01-28 13:43:31 -0500

    initial commit setup simple web proj

---
+++ web/components/test-button/test-button.tsx
@@ -0,0 +1,11 @@
+import "./test-button.less";
+
+interface TestButtonProps
+{
+
+}
+
+export function TestButton(props:TestButtonProps):JSX.Element
+{
+  return <div className="test-button">hello</div>;
+}
