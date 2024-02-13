7f5dd5b9e971f4d9e3320294238fa7eebedfd8ac
risonakamo <hmykanae@gmail.com>
2024-02-13 02:18:27 -0500

    trying to mutate but not working

--- web/pages/time-blocks/time-blocks-index.tsx
+++ web/pages/time-blocks/time-blocks-index.tsx
@@ -11,7 +11,7 @@ import { useMutation } from "@tanstack/react-query";
 
 import {TimeBlock} from "components/time-block/time-block";
 
-import { getTimeblocks,newTimeblock,toggleTimeblock } from "apis/time-block-api";
+import { getTimeblocks,newTimeblock,toggleTimeblock,setTimeblockTitle } from "apis/time-block-api";
 import { apiTimeblockConvert } from "lib/api_convert";
 
 import "./time-blocks-index.less";
@@ -38,6 +38,7 @@ function TimeBlocksIndex():JSX.Element
 
 
   // --- mut funcs ---
+  // request to create a timeblock. triggers refresh of timeblock data
   const createTimeblockMqy=useMutation({
     mutationKey:["create-timeblock"],
 
@@ -46,13 +47,13 @@ function TimeBlocksIndex():JSX.Element
       return newTimeblock();
     },
 
-    onSuccess(data:string):void
+    onSuccess():void
     {
-      console.log("create succeeded with msg:",data);
       timeblocksQy.refetch();
     }
   });
 
+  // request to toggle a timeblock. triggers refresh of timeblock data.
   const toggleTimeblockMqy=useMutation({
     mutationKey:["start-timeblock"],
 
@@ -64,6 +65,54 @@ function TimeBlocksIndex():JSX.Element
     onSuccess():void
     {
       timeblocksQy.refetch();
+    }
+  });
+
+  interface SetTitleMqyArgs
+  {
+    blockId:string
+    newTitle:string
+  }
+
+  // request to change title of a block. pre-emptively tries to modify the timeblocks
+  // data to reflect the change.
+  const setTitleMqy=useMutation({
+    mutationKey:["set-title"],
+
+    async mutationFn(args:SetTitleMqyArgs):Promise<string>
+    {
+      console.log("what");
+      // await qyClient.cancelQueries({
+      //   queryKey:["timeblocks"]
+      // });
+
+      qyClient.setQueryData<TimeBlocks>(["timeblocks"],(prev:TimeBlocks|undefined):TimeBlocks=>{
+        var clone:TimeBlocks={};
+        console.log("prev",prev);
+
+        console.log("in");
+        if (prev)
+        {
+          console.log("hello?");
+          clone=_.cloneDeep(prev);
+        }
+
+        console.log("how");
+        console.log(clone);
+        clone[args.blockId].title=args.newTitle;
+        console.log("new clone",clone);
+
+        return clone;
+      });
+
+      console.log("?");
+
+      return setTimeblockTitle(args.blockId,args.newTitle);
+    },
+
+    onSettled():void
+    {
+
     }
   });
 
@@ -83,10 +132,13 @@ function TimeBlocksIndex():JSX.Element
   function r_timeblocks():JSX.Element[]
   {
     return _.map(_.values(timeblocksQy.data),(timedata:TimeBlockData,i:number):JSX.Element=>{
-      // all time blocks need their own handlers
+      /** todo: this still seems bad. request to change title on title change */
       function h_titleChange(newtitle:string):void
       {
-
+        setTitleMqy.mutate({
+          blockId:timedata.id,
+          newTitle:newtitle
+        });
       }
 
       /** clicked timer start button. trigger timeblock toggle */
